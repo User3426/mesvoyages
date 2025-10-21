63e4d8472e7a6e2f362e7c1463fc4fbe198769ac
User3426 <tristanrage.adresse@gmail.com>
2025-10-21 18:23:48 +0200

    Phase 23 : tests

---
+++ tests/VisiteTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Tests;
+
+use App\Entity\Environnement;
+use App\Entity\Visite;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of VisiteTest
+ *
+ * @author emds
+ */
+class VisiteTest extends TestCase {
+
+    public function testGetDatecreationString(){
+        $visite = new Visite();
+        $visite->setDatecreation(new DateTime("2024-04-24"));
+        $this->assertEquals("24/04/2024", $visite->getDatecreationString());
+    }
+    
+        public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
+    }
+    
+}
